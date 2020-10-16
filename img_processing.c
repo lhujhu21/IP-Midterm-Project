2db6d421da3ea69e3d301e29c34f13febc74c6c4
lhujhu21 <lhujhu21@gmail.com>
2020-10-15 21:54:05 -0400

    Implemented binarize function

--- img_processing.c
+++ img_processing.c
@@ -18,9 +18,30 @@ Image* grayscale(Image* im) {
   for (int i = 0; i < (im->rows * im->cols); i++) {
     Pixel p = im->data[i];
     float gray = 0.3 * p->r + 0.59 * p->g + 0.11 * p->b;
-    out->data[i]->r = p;
-    out->data[i]->g = p;
-    out->data[i]->b = p;
+    out->data[i]->r = gray;
+    out->data[i]->g = gray;
+    out->data[i]->b = gray;
+  }
+  return out;
+}
+
+Image* binarize(Image* im, int threshold) {
+  // Check if threshold value is valid
+  if (threshold <= 0 || threshold >= 255) {
+    return NULL; 
+  }  
+  Image* out = grayscale(input);
+  for (int i = 0; i < (out->rows * out->cols); i++) {
+    float gray = out->data[i]->r;
+    if (gray < threshold) {
+      out->data[i]->r = 0;
+      out->data[i]->g = 0;
+      out->data[i]->b = 0;
+    } else {
+      out->data[i]->r = 255;
+      out->data[i]->g = 255;
+      out->data[i]->b = 255;
+    }
   }
   return out;
 }
