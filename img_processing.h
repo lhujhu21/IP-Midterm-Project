02662a09adba69d28e8bc5e3cb1f3136e4cc9f5c
lhujhu21 <lhujhu21@gmail.com>
2020-10-15 23:51:50 -0400

    Added function declarations for Grayscale(), Binarize(), and CreateImage()

--- img_processing.h
+++ img_processing.h
@@ -1,1 +1,25 @@
-// Header file for image processing stuff
+// Lucy Hu, lhu15
+// Morgan Wu, mwu69
+
+/**
+ * Creates a new Image struct.
+ * @parameter rows the number of rows in image.
+ * @paramater cols the number of columns in image.
+ * @return pointer to the new struct.
+ */
+Image* CreateImage(int rows; int cols);
+
+/**
+ * Converts image to grayscale.
+ * @parameter im Image pointer to be converted.
+ * @return pointer to the grayscale image struct.
+ */
+Image* Grayscale(Image* im);
+
+/**
+ * Binarizes image.
+ * @parameter im Image pointer to be binarized.
+ * @parameter threshold value to check against.
+ * @return pointer to binarized image struct.
+ */
+Image* Binarize(Image* im, int threshold) {
