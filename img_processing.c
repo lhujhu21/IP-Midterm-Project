a7528f0a79c0de50480592351bb99cdd624eca54
lhujhu21 <lhujhu21@gmail.com>
2020-10-15 21:25:33 -0400

    Implemented grayscale function

--- img_processing.c
+++ img_processing.c
@@ -2,6 +2,7 @@
 
 // Grayscale function
 Image* grayscale(Image* im) {
+  // Create Image struct and allocate memory for output photo
   Image* out = malloc(sizeof(Image));
   out->rows = im->rows;
   out->cols = im->cols;
@@ -10,10 +11,9 @@ Image* grayscale(Image* im) {
   for (int i = 0; i < (im->rows * im->cols); i++) {
     Pixel p = im->data[i];
     float gray = 0.3 * p->r + 0.59 * p->g + 0.11 * p->b;
-    p->r = gray;
-    p->g = gray;
-    p->b = gray;
-    out->data = p;
+    out->data[i]->r = p;
+    out->data[i]->g = p;
+    out->data[i]->b = p;
   }
   return out;
 }
